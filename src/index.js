9bfef189e2a38ae53a4151175c3ce780dace40ca
hosalt <hosalt@qq.com>
2018-07-27 21:52:23 +0800

    init make-redux

---
+++ src/index.js
@@ -0,0 +1,66 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// // import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// // registerServiceWorker();
+
+const appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书',
+    color: 'blue',
+  }
+}
+
+function renderApp (appState) {
+  renderTitle(appState.title)
+  renderContent(appState.content)
+}
+
+function renderTitle (title) {
+  const {text, color} = title
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = text
+  titleDOM.style.color = color
+}
+
+function renderContent (content) {
+  const contentDOM = document.getElementById('content')
+  const {text, color} = content
+  contentDOM.innerHTML = text
+  contentDOM.style.color = color
+}
+
+// 修改数据入口（所有的数据的修改必须通过此函数）
+function dispatch (action) {
+  const {type, color, text} = action
+  switch (type) {
+    case 'UPDATE_TITLE_TEXT':
+      appState.title.text = text
+      break
+    case 'UPDATE_TITLE_COLOR':
+      appState.title.color = color
+      break
+    default:
+      break
+  }
+}
+
+function createStore (state, stateChanger) {
+  const getState = () => state
+  const dispatch = (action) => stateChanger(state, action)
+  return{getState, dispatch}
+}
+
+renderApp(appState)
+
+dispatch({type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》'})
+dispatch({type: 'UPDATE_TITLE_COLOR', color: 'blue'})
+
+renderApp(appState)
